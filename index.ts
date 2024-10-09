8ba0ed427b1ff3b60ffd19ebbea14fc75aa7f616
Iryna Kostromytska <Iryna.Kostromytska@dataart.com>
2024-10-09 09:35:09 +0300

    feat: connection with DB and UserCredentialsSchema

--- index.ts
+++ index.ts
@@ -1,6 +1,27 @@
 import express from 'express';
-import cloudinary from "./helpers/cloudinary.js";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => console.log('Server is runing!'));
+const corsMiddleware = cors();
+app.use(corsMiddleware);
+
+const { DB_HOST, PORT } = process.env;
+if (!DB_HOST) {
+    console.error('DB_HOST is not defined in environment variables');
+    process.exit(1);
+  }
+
+mongoose.connect(DB_HOST)
+.then(()=> {
+    app.listen(PORT, () => console.log('Server is runing!'));
+})
+.catch(error => {
+    console.log(error.message)
+    process.exit(1)
+})
+
---
+++ models/UserCredentials.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+import {emailRegexp} from "../constants/regexp";
+
+const userCredentialsSchema = new Schema({
+    firstName: {
+        type: String,
+        minlength: 2,
+        maxlength: 20,
+        required: [true, "firstName is required"]
+    },
+    lastName: {
+        type: String,
+        minlength: 2,
+        maxlength: 20,
+        required: [true, "lastName is required"]
+    },
+    password: {
+        type: String,
+        minlength: 8,
+        maxlength: 20,
+        required: [true, "Password is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        match: emailRegexp,
+        unique: true,
+    },
+    token: {
+        type: String,
+        default: null,
+    },
+
+})
+
+const UserCredentials = model("userCredentials", userCredentialsSchema);
+
+export default UserCredentials;
