5e03b2896cba169d630f23b2b64415c9e01d4c9f
IamLoren <94172173+IamLoren@users.noreply.github.com>
2024-10-11 11:17:29 +0300

    Delete build directory

--- build/middleware/autenticate.d.ts
+++
@@ -1,7 +0,0 @@
-import { JwtPayload } from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
-interface AuthenticatedRequest extends Request {
-    user?: JwtPayload;
-}
-export declare const authenticate: (req: AuthenticatedRequest, res: Response, next: NextFunction) => Promise<void>;
-export {};
