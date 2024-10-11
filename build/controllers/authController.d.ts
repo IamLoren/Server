53fcc623876cc98090366955febcbb00538fb557
IamLoren <94172173+IamLoren@users.noreply.github.com>
2024-10-11 11:19:24 +0300

    Delete build directory

--- build/controllers/authController.d.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-import { RegisterReq, RegisterRes } from '../types/authTypes.js';
-declare const _default: {
-    register: (req: RegisterReq, res: RegisterRes, next: NextFunction) => Promise<void>;
-    login: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-    getCurrent: (req: any, res: any) => Promise<void>;
-};
-export default _default;
--- build/helpers/validateBody.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, NextFunction } from 'express';
-import { Schema } from 'joi';
-declare const validateBody: (schema: Schema) => (req: Request, _: any, next: NextFunction) => void;
-export default validateBody;
