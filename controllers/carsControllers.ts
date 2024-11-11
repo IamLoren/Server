5084814bcb7368adcf834c8ca00b4d2d34625b16
Iryna Kostromytska <Iryna.Kostromytska@dataart.com>
2024-11-11 11:54:49 +0200

    fix: fixed added car availability

--- controllers/carsControllers.ts
+++ controllers/carsControllers.ts
@@ -5,8 +5,8 @@ import UserCredentials from '../models/UserCredentials'
 
 interface CustomRequest extends Request {
     user?: {
-        jwtPayload: string; 
-    };
+        jwtPayload: string
+    }
 }
 
 const getAllCars = async (req: Request, res: Response, next: NextFunction) => {
@@ -24,7 +24,11 @@ const getAllCars = async (req: Request, res: Response, next: NextFunction) => {
     }
 }
 
-const updateCar = async (req:CustomRequest, res:Response, next:NextFunction) => {
+const updateCar = async (
+    req: CustomRequest,
+    res: Response,
+    next: NextFunction
+) => {
     try {
         if (!req.hasOwnProperty('user')) {
             throw new Error('Request doesnt have necessary property `user` ')
@@ -67,7 +71,11 @@ const updateCar = async (req:CustomRequest, res:Response, next:NextFunction) => 
     }
 }
 
-const updateAvailability = async (req:CustomRequest, res:Response, next:NextFunction) => {
+const updateAvailability = async (
+    req: CustomRequest,
+    res: Response,
+    next: NextFunction
+) => {
     try {
         if (!req.hasOwnProperty('user')) {
             throw new Error('Request doesnt have necessary property `user` ')
@@ -88,8 +96,8 @@ const updateAvailability = async (req:CustomRequest, res:Response, next:NextFunc
         if (admin.role === 'admin') {
             const carId = req.params.id
             const objectId = new mongoose.Types.ObjectId(carId)
-            const orderId = req.body.orderId;
- 
+            const orderId = req.body.orderId
+
             const car = await Car.findOne({
                 _id: objectId,
             })
@@ -100,11 +108,69 @@ const updateAvailability = async (req:CustomRequest, res:Response, next:NextFunc
                 )
             }
 
-            car.availability.pull({ orderId });
+            car.availability.pull({ orderId })
             await car.save()
-            res.status(200).json({ message: "car availability was updated successfully" })
+            res.status(200).json({
+                message: 'car availability was updated successfully',
+            })
         } else {
             res.status(403)
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+const changeAvailability = async (
+    req: CustomRequest,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        if (!req.hasOwnProperty('user')) {
+            throw new Error('Request doesnt have necessary property `user` ')
+        }
+        if (!req.user?.hasOwnProperty('jwtPayload')) {
+            throw new Error(
+                'Request doesnt have necessary property `user.jwtPayload` '
+            )
+        }
+        const userId = req.user.jwtPayload
+        const objectId = new mongoose.Types.ObjectId(userId)
+        const admin = await UserCredentials.findOne({ _id: objectId })
+
+        if (!admin) {
+            throw new Error('admin with such id doesnt exist')
+        }
+
+        if (admin.role === 'admin') {
+            const carId = req.params.id
+            const objectId = new mongoose.Types.ObjectId(carId)
+            const { orderId, startDate, endDate } = req.body
+
+            const updatedCar = await Car.findOneAndUpdate(
+                { _id: objectId, 'availability.orderId': orderId },
+                {
+                    $set: {
+                        'availability.$.startDate': startDate,
+                        'availability.$.endDate': endDate,
+                    },
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            )
+
+            if (!updatedCar) {
+                res.status(404).json({
+                    message: 'Order not found or car not updated',
+                })
+            } else {
+                res.status(200).json({ updatedCar })
+            }
+        } else {
+            res.status(403).json({ message: 'Forbidden' })
         }
     } catch (error) {
         next(error)
@@ -115,4 +181,5 @@ export default {
     getAllCars,
     updateCar,
     updateAvailability,
+    changeAvailability,
 }
